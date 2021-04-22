474c378cef4efc44c6d7b5ad5b5465da15058a6d
tidiview <francois.vidit@laposte.net>
2021-04-22 12:00:12 +0200

    added corrections

--- examples/src/components/RichTextEditor_187.svelte
+++ examples/src/components/RichTextEditor_187.svelte
@@ -5,46 +5,57 @@
   import { createExtendedThreeRichTextEditor, clear, toHTML, toPlainText, toJSON } from "../../../state";
 
   const html = `
-<div id="body-inner">
-<h1>The Mona Lisa according to Giorgio Vasari</h1>
-<figure><picture><source sizes="(max-width: 767px) 98vw, (min-width: 959px) 50vw, 86vw" srcset="
-https://francois-vidit.com/user/sites/docs/pages/01.home/01.paris/01.louvre/01.la-joconde/01.vasari/la-joconde-280.webp 280w,
-https://francois-vidit.com/user/sites/docs/pages/01.home/01.paris/01.louvre/01.la-joconde/01.vasari/la-joconde-380.webp 380w,
-https://francois-vidit.com/user/sites/docs/pages/01.home/01.paris/01.louvre/01.la-joconde/01.vasari/la-joconde-480.webp 480w,
-https://francois-vidit.com/user/sites/docs/pages/01.home/01.paris/01.louvre/01.la-joconde/01.vasari/la-joconde-640.webp 640w,
-https://francois-vidit.com/user/sites/docs/pages/01.home/01.paris/01.louvre/01.la-joconde/01.vasari/la-joconde-840.webp 840w,
-https://francois-vidit.com/user/sites/docs/pages/01.home/01.paris/01.louvre/01.la-joconde/01.vasari/la-joconde-1280.webp 1280w,
-https://francois-vidit.com/user/sites/docs/pages/01.home/01.paris/01.louvre/01.la-joconde/01.vasari/la-joconde-1600.webp 1600w,
-https://francois-vidit.com/user/sites/docs/pages/01.home/01.paris/01.louvre/01.la-joconde/01.vasari/la-joconde-1920.webp 1920w" type="image/webp"><img src="https://francois-vidit.com/user/sites/docs/pages/01.home/01.paris/01.louvre/01.la-joconde/01.vasari/la-joconde-840.jpg" title="Portrait of Lisa Gerardini, spouse of Francesco del Giocondo said «Mona Lisa, la Gioconda» or «the Joconde»" alt="Portrait of Lisa Gerardini, spouse of Francesco del Giocondo said «Mona Lisa, la Gioconda» or «the Joconde»" class="class-diane-img" sizes="(max-width: 767px) 98vw, (min-width: 959px) 50vw, 86vw" srcset="
-https://francois-vidit.com/user/sites/docs/pages/01.home/01.paris/01.louvre/01.la-joconde/01.vasari/la-joconde-280.jpg 280w,
-https://francois-vidit.com/user/sites/docs/pages/01.home/01.paris/01.louvre/01.la-joconde/01.vasari/la-joconde-380.jpg 380w,
-https://francois-vidit.com/user/sites/docs/pages/01.home/01.paris/01.louvre/01.la-joconde/01.vasari/la-joconde-480.jpg 480w,
-https://francois-vidit.com/user/sites/docs/pages/01.home/01.paris/01.louvre/01.la-joconde/01.vasari/la-joconde-640.jpg 640w,
-https://francois-vidit.com/user/sites/docs/pages/01.home/01.paris/01.louvre/01.la-joconde/01.vasari/la-joconde-840.jpg 840w,
-https://francois-vidit.com/user/sites/docs/pages/01.home/01.paris/01.louvre/01.la-joconde/01.vasari/la-joconde-1280.jpg 1280w,
-https://francois-vidit.com/user/sites/docs/pages/01.home/01.paris/01.louvre/01.la-joconde/01.vasari/la-joconde-1600.jpg 1600w,
-https://francois-vidit.com/user/sites/docs/pages/01.home/01.paris/01.louvre/01.la-joconde/01.vasari/la-joconde-1920.jpg 1920w" id="zephyr_et_flore"></picture><figcaption>Portrait of Lisa Gerardini, spouse of Francesco del Giocondo said «Mona Lisa, la Gioconda» or «the Joconde»</figcaption></figure>
+<article>
+
+<h1><cite>The Mona Lisa</cite> according to Giorgio Vasari</h1>
+
+<figure>
+  <picture><source sizes="(max-width: 767px) 98vw, (min-width: 959px) 50vw, 86vw" srcset="
+  https://francois-vidit.com/user/sites/docs/pages/01.home/01.paris/01.louvre/01.la-joconde/01.vasari/la-joconde-280.webp 280w,
+  https://francois-vidit.com/user/sites/docs/pages/01.home/01.paris/01.louvre/01.la-joconde/01.vasari/la-joconde-380.webp 380w,
+  https://francois-vidit.com/user/sites/docs/pages/01.home/01.paris/01.louvre/01.la-joconde/01.vasari/la-joconde-480.webp 480w,
+  https://francois-vidit.com/user/sites/docs/pages/01.home/01.paris/01.louvre/01.la-joconde/01.vasari/la-joconde-640.webp 640w,
+  https://francois-vidit.com/user/sites/docs/pages/01.home/01.paris/01.louvre/01.la-joconde/01.vasari/la-joconde-840.webp 840w,
+  https://francois-vidit.com/user/sites/docs/pages/01.home/01.paris/01.louvre/01.la-joconde/01.vasari/la-joconde-1280.webp 1280w,
+  https://francois-vidit.com/user/sites/docs/pages/01.home/01.paris/01.louvre/01.la-joconde/01.vasari/la-joconde-1600.webp 1600w,
+  https://francois-vidit.com/user/sites/docs/pages/01.home/01.paris/01.louvre/01.la-joconde/01.vasari/la-joconde-1920.webp 1920w" type="image/webp"><img src="https://francois-vidit.com/user/sites/docs/pages/01.home/01.paris/01.louvre/01.la-joconde/01.vasari/la-joconde-840.jpg" title="Portrait of Lisa Gerardini, spouse of Francesco del Giocondo said «Mona Lisa, la Gioconda» or «the Joconde»" alt="Portrait of Lisa Gerardini, spouse of Francesco del Giocondo said «Mona Lisa, la Gioconda» or «the Joconde»" class="class-diane-img" sizes="(max-width: 767px) 98vw, (min-width: 959px) 50vw, 86vw" srcset="
+  https://francois-vidit.com/user/sites/docs/pages/01.home/01.paris/01.louvre/01.la-joconde/01.vasari/la-joconde-280.jpg 280w,
+  https://francois-vidit.com/user/sites/docs/pages/01.home/01.paris/01.louvre/01.la-joconde/01.vasari/la-joconde-380.jpg 380w,
+  https://francois-vidit.com/user/sites/docs/pages/01.home/01.paris/01.louvre/01.la-joconde/01.vasari/la-joconde-480.jpg 480w,
+  https://francois-vidit.com/user/sites/docs/pages/01.home/01.paris/01.louvre/01.la-joconde/01.vasari/la-joconde-640.jpg 640w,
+  https://francois-vidit.com/user/sites/docs/pages/01.home/01.paris/01.louvre/01.la-joconde/01.vasari/la-joconde-840.jpg 840w,
+  https://francois-vidit.com/user/sites/docs/pages/01.home/01.paris/01.louvre/01.la-joconde/01.vasari/la-joconde-1280.jpg 1280w,
+  https://francois-vidit.com/user/sites/docs/pages/01.home/01.paris/01.louvre/01.la-joconde/01.vasari/la-joconde-1600.jpg 1600w,
+  https://francois-vidit.com/user/sites/docs/pages/01.home/01.paris/01.louvre/01.la-joconde/01.vasari/la-joconde-1920.jpg 1920w"></picture><figcaption>Portrait of Lisa Gerardini, spouse of Francesco del Giocondo said «Mona Lisa, la Gioconda» or «the Joconde»</figcaption>
+</figure>
+
 <div class="notices green">
-<p>The description of the <a href="https://en.wikipedia.org/wiki/Mona_Lisa" title="https://en.wikipedia.org/wiki/Mona_Lisa">Mona Lisa</a> presented below was written by <a href="https://en.wikipedia.org/wiki/Giorgio_Vasari" title="https://en.wikipedia.org/wiki/Giorgio_Vasari">Giorgio VASARI</a> and published in Florence in 1550.<br>
-It is the best known, oldest and most reliable of these to date. As such, it has influenced most of the subsequent descriptions.<br>
-Vasari describes a painting that he himself has not seen because the painting is then in Fontainebleau where Vasari never went.<br>
-The description tells us about the reputation of the work: it was performed in Florence in 1503, <a href="https://en.wikipedia.org/wiki/Leonardo_da_Vinci" title="https://en.wikipedia.org/wiki/Leonardo_da_Vinci">Leonardo da Vinci</a> worked there for 4 years, it represents Francesco Del Giocondo's wife, referred to as “M[ad]on[n]a Lisa”, “Madame Lise”.<br>
-But moreover, it allows us to get what was making its value in the eyes of Vasari.</p>
+<p>The description of <a href="https://en.wikipedia.org/wiki/Mona_Lisa" title="https://en.wikipedia.org/wiki/Mona_Lisa"><cite>The Mona Lisa</cite></a> presented below was written by <a href="https://en.wikipedia.org/wiki/Giorgio_Vasari" title="https://en.wikipedia.org/wiki/Giorgio_Vasari">Giorgio VASARI</a> and published in Florence in <time datetime="1550">1550</time>.</p>
+<p>It is the best known, oldest and most reliable of these to date. As such, it has influenced most of the subsequent descriptions.</p>
+<p>Vasari describes a painting that he himself has not seen because the painting is then in Fontainebleau where Vasari never went.</p>
+<p>The description tells us about the reputation of the work: it was performed in Florence in <time datetime="1503">1503</time>, <a href="https://en.wikipedia.org/wiki/Leonardo_da_Vinci" title="https://en.wikipedia.org/wiki/Leonardo_da_Vinci">Leonardo da Vinci</a> worked there for <time datetime="P4Y">4 years</time>, it represents Francesco Del Giocondo's wife, referred to as “M[ad]on[n]a Lisa”, “Madame Lise”.</p>
+<p>But moreover, it allows us to get what was making its value in the eyes of Vasari.</p>
 </div>
-<p>Leonardo undertook to execute, for Francesco del Giocondo, the portrait of Mona Lisa, his wife, and after he had lingered over it for four years, he left it unfinished;
-“and the work is today in the possession of King Francis of France, at Fontainebleau.<br>
-Anyone wishing to see <span style="color: #3d8b3d;">the degree to which art could imitate nature could readily perceive this from the head;  since therein are counterfeited all those minutenesses that with subtlety are able to be painted</span>:
+
+<h2 id="vasari" class="yellow"><cite>The Mona Lisa</cite> according to text of Giogio Vasari, <cite>Lives of the Painters, Sculptors and Architects</cite></h2>
+
+<p>Leonardo undertook to execute, for Francesco del Giocondo, the portrait of <mark>Mona Lisa</mark>, his wife, and after he had lingered over it for four years, he left it unfinished;
+“and the work is today in the possession of King Francis of France, at Fontainebleau.</p>
+<p>Anyone wishing to see <span style="color: #3d8b3d;">the degree to which art could imitate nature could readily perceive this from the head;  since therein are counterfeited all those minutenesses that with subtlety are able to be painted</span>:
 seeing that the eyes had that lustre and moistness which are always seen in the living creature,
-and around them were the lashes and all those rosy and pearly tints that demand the greatest delicacy of execution.<br>
-The eyebrows, through his having shown the manner in which the hairs spring from the flesh, here more close and here more scanty, and curve according to the pores of the flesh, could not be more natural.<br>
-The nose, with its beautiful nostrils, rosy and tender, appeared to be alive.<br>
-The mouth with its opening, and with its ends united by the red of the lips to the flesh-tints of the face, seemed, in truth, to be not colours but flesh.<br>
-In the pit of the throat, if one gazed upon it intently, could be seen the beating of the pulse:
-and indeed it may be said that it was painted in such a manner as to make every brave artificer, be he who he may, tremble and lose courage.<br>
-He employed also this device: <span style="color: #3d8b3d;">Mona Lisa being very beautiful, while he was painting her portrait, he retained those who played or sang, and continually jested, who would make her to remain merry, in order to take away that melancholy which painters are often wont to give to their portraits</span>.<br>
-<span style="color: #3d8b3d;">And in this work of Leonardo there was a smile so pleasing, that it was a thing more divine than human to behold, and it was held to be something marvelous, in that it was not other than alive</span>.</p>
-<p><a href="https://en.wikipedia.org/wiki/Giorgio_Vasari" title="https://en.wikipedia.org/wiki/Giorgio_Vasari" class="green">Giorgio Vasari</a><span style="color: #3d8b3d;">, “Lives of the Painters, Sculptors and Architects”, book IV.</span></p>
-</div>
+and around them were the lashes and all those rosy and pearly tints that demand the greatest delicacy of execution.</p>
+<p>The eyebrows, through his having shown the manner in which the hairs spring from the flesh, here more close and here more scanty, and curve according to the pores of the flesh, could not be more natural.</p>
+<p>The nose, with its beautiful nostrils, rosy and tender, appeared to be alive.</p>
+<p>The mouth with its opening, and with its ends united by the red of the lips to the flesh-tints of the face, seemed, in truth, to be not colours but flesh.</p>
+<p>In the pit of the throat, if one gazed upon it intently, could be seen the beating of the pulse:
+and indeed it may be said that it was painted in such a manner as to make every brave artificer, be he who he may, tremble and lose courage.</p>
+<p>He employed also this device: <span style="color: #3d8b3d;"><mark>Mona Lisa</mark> being very beautiful, while he was painting her portrait, he retained those who played or sang, and continually jested, who would make her to remain merry, in order to take away that melancholy which painters are often wont to give to their portraits</span>.</p>
+
+<footer>
+  <p><span style="color: #3d8b3d;">And in this work of Leonardo there was a smile so pleasing, that it was a thing more divine than human to behold, and it was held to be something marvelous, in that it was not other than alive</span>.</p>
+  <p><a href="https://en.wikipedia.org/wiki/Giorgio_Vasari" title="https://en.wikipedia.org/wiki/Giorgio_Vasari" class="green">Giorgio Vasari</a><span style="color: #3d8b3d;">, <cite>Lives of the Painters, Sculptors and Architects</cite>, book IV.</span></p>
+</footer>
+</article>
 `;
 
   let focusEditor;
