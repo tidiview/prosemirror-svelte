9f59d4d60462ef796f5a644f9d860e96f2923314
tidiview <francois.vidit@laposte.net>
2021-04-29 18:04:19 +0200

    added <abbr>, .top-space, p.conversation, <q> in <figcaption>

--- examples/src/components/RichTextEditor_202.svelte
+++ examples/src/components/RichTextEditor_202.svelte
@@ -7,7 +7,10 @@
   const html = `
 <article>
 
+<hgroup>
 <h1 class="yellow">Perseus</h1>
+<h2 class="yellow">at the Chateau de Cheverny</h2>
+</hgroup>
 
 <figure>
   <picture><source media="(min-width: 959px)" sizes="(max-width: 767px) 98vw, (min-width: 959px) 50vw, 86vw" srcset="
@@ -30,7 +33,7 @@
   https://francois-vidit.com/user/sites/docs/pages/01.home/04.chateaux-de-la-loire/03.cheverny/02.perseus/persee-focus-380.jpg 380w,
   https://francois-vidit.com/user/sites/docs/pages/01.home/04.chateaux-de-la-loire/03.cheverny/02.perseus/persee-focus-480.jpg 480w,
   https://francois-vidit.com/user/sites/docs/pages/01.home/04.chateaux-de-la-loire/03.cheverny/02.perseus/persee-focus-640.jpg 640w,
-  https://francois-vidit.com/user/sites/docs/pages/01.home/04.chateaux-de-la-loire/03.cheverny/02.perseus/persee-focus_700x763.jpg 700w"></picture><figcaption>Perseus helps Andromeda chained to a rock</figcaption>
+  https://francois-vidit.com/user/sites/docs/pages/01.home/04.chateaux-de-la-loire/03.cheverny/02.perseus/persee-focus_700x763.jpg 700w"></picture><figcaption>Perseus <q cite="#persee">in swift flight through empty space</q> helps Andromeda chained to a rock</figcaption>
 </figure>
 
 
@@ -41,51 +44,62 @@
   </div>
 
 
-  <h2 class="yellow"><cite>Perseus and Andromeda</cite> according to <cite>The Metamorphoses</cite></h2>
+  <h2 id="persee" class="yellow"><cite>Perseus and Andromeda</cite> according to the text of Book <abbr title="four">IV</abbr> of <cite>The Metamorphoses</cite></h2>
 
-    <ol class="doc">
-      <li>Aeolus, son of Hippotas, had confined the winds in their prison under Mount Etna, and Lucifer, who exhorts us to work, shone brightest of all</li>
-      <li value="665">
-      in the depths of the eastern sky.</li>
+    <ol class="doc"><li>Aeolus, son of Hippotas, had confined the winds in their prison under Mount Etna, and Lucifer, who exhorts us to work, shone brightest of all</li>
+      <li value="665">in the depths of the eastern sky.</li>
       <li>Perseus strapped the winged sandals he had put to one side to his feet, armed himself with his curved sword, and cut through the clear air on beating pinions.</li>
       <li>Leaving innumerable nations behind, below and around him, he came in sight of the Ethiopian peoples, and the fields of Cepheus.</li>
       <li value="670">There Jupiter Ammon had unjustly ordered the innocent Andromeda to pay the penalty for her mother Cassiopeia’s words.</li>
-      <li>As soon as Perseus, great-grandson of Abas, saw her fastened by her arms to the hard rock,</li>
-      <li value="675">
-      he would have thought she was a marble statue, except that a light breeze stirred her hair, and warm tears ran from her eyes.</li>
+
+      <li class="top-space">As soon as Perseus, great-grandson of Abas, saw her fastened by her arms to the hard rock,</li>
+      <li value="675">he would have thought she was a marble statue, except that a light breeze stirred her hair, and warm tears ran from her eyes.</li>
       <li>He took fire without knowing it and was stunned, and seized by the vision of the form he saw, he almost forgot to flicker his wings in the air.</li>
-      <li>As soon as he had touched down, he said ‘O, you do not deserve these chains, but those that link ardent lovers together.</li>
-      <li value="680">Tell me your name, I wish to know it, and the name of your country, and why you are wearing these fetters.</li>
+      <li>As soon as he had touched down, he said
+        <p class="conversation">‘O, you do not deserve these chains, but those that link ardent lovers together.</p>
+      </li>
+      <li value="680">
+        <p class="conversation">Tell me your name, I wish to know it, and the name of your country, and why you are wearing these fetters.’</p>
+      </li>
       <li>At first she was silent: a virgin, she did not dare to address a man, and she would have hidden her face modestly with her hands, if they had not been fastened behind her.</li>
       <li>She used her eyes instead, and they filled with welling tears.</li>
       <li value="685">At his repeated insistence, so as not to seem to be acknowledging a fault of her own, she told him her name and the name of her country, and what faith her mother had had in her own beauty.</li>
-      <li>Before she had finished speaking, all the waves resounded, and a monster menaced them, rising from the deep sea, and</li>
-      <li value="690">
-      covered the wide waters with its breadth.</li>
+
+      <li class="top-space">Before she had finished speaking, all the waves resounded, and a monster menaced them, rising from the deep sea, and</li>
+      <li value="690">covered the wide waters with its breadth.</li>
       <li>The girl cried out: her grieving father and mother were together nearby, both wretched, but the mother more justifiably so.</li>
       <li>They bring no help with them, only weeping and lamentations to suit the moment, and cling to her fettered body.</li>
-      <li value="695">Then the stranger speaks ‘There will be plenty of time left for tears, but only a brief hour is given us to work.</li>
-      <li>If I asked for this girl as Perseus, son of Jupiter and that Danaë, imprisoned in the brazen tower, whom Jupiter filled with his rich golden shower; Perseus conqueror of the Gorgon with snakes for hair, he who dared to fly, driven through the air,</li>
+      <li value="695">Then the stranger speaks
+        <p class="conversation">‘There will be plenty of time left for tears, but only a brief hour is given us to work.</p>
+      </li>
+      <li>
+        <p class="conversation">If I asked for this girl as Perseus, son of Jupiter and that Danaë, imprisoned in the brazen tower, whom Jupiter filled with his rich golden shower; Perseus conqueror of the Gorgon with snakes for hair, he who dared to fly, driven through the air,</p>
+      </li>
       <li value="700">
-      on soaring wings, then surely I should be preferred to all other suitors as a son-in-law.</li>
-      <li>If the gods favour me, I will try to add further merit to these great gifts. I will make a bargain.</li>
-      <li>Rescued by my courage, she must be mine.’</li>
+        <p class="conversation">on soaring wings, then surely I should be preferred to all other suitors as a son-in-law.</p>
+      </li>
+      <li>
+        <p class="conversation">If the gods favour me, I will try to add further merit to these great gifts. I will make a bargain.</p>
+      </li>
+      <li>
+        <p class="conversation">Rescued by my courage, she must be mine.’</p>
+      </li>
       <li>Her parents accept the contract (who would hesitate?) and, entreating him,</li>
-      <li value="705">
-      promise a kingdom, as well, for a dowry.</li>
-      <li>See how the creature comes, parting the waves with surging breast, like a fast ship with pointed prow ploughing the water, driven by the sweat-covered muscles of her crew.</li>
+      <li value="705">promise a kingdom, as well, for a dowry.</li>
+
+      <li class="top-space">See how the creature comes, parting the waves with surging breast, like a fast ship with pointed prow ploughing the water, driven by the sweat-covered muscles of her crew.</li>
       <li value="710">It was as far from the rock as a Balearic sling can send a lead shot through the air, when suddenly the young hero, pushing his feet hard against the earth, shot high among the clouds.</li>
       <li>When the shadow of a man appeared on the water’ surface, the creature raged against the shadow it had seen.</li>
       <li value="715">As Jupiter’s eagle, when it sees a snake, in an open field, showing its livid body to the sun, takes it from behind, and fixes its eager talons in the scaly neck, lest it twists back its cruel fangs, so the descendant of Inachus hurling himself headlong, in swift flight, through empty space, attacked the creature’s back, and, as it roared, buried his sword, to the end of the curved blade,</li>
-      <li value="720">
-      in the right side of its neck.</li>
+      <li value="720">in the right side of its neck.</li>
       <li>Hurt by the deep wound, now it reared high in the air, now it dived underwater, or turned now, like a fierce wild boar, when the dogs scare him, and the pack is baying around him.</li>
       <li value="725">Perseus evades the eager jaws on swift wings, and strikes with his curved sword wherever the monster is exposed, now at the back encrusted with barnacles, now at the sides of the body, now where the tail is slenderest, ending fishlike.</li>
       <li>The beast vomits seawater mixed with purplish blood.</li>
       <li value="730"> The pinions grow heavy, soaked with spray.</li>
       <li>Not daring to trust his drenched wings any further, he sees a rock whose highest point stands above quiet water, hidden by rough seas.</li>
       <li>Resting there, and holding on to the topmost pinnacle with his left hand, he drives his sword in three or four times, repeatedly.</li>
-      <li>The shores, and the high places of the gods,</li>
+
+      <li class="top-space">The shores, and the high places of the gods,</li>
       <li value="735"> fill with the clamour of applause.</li>
       <li>Cassiope and Cepheus rejoice, and greet their son-in-law, acknowledging him as the pillar of their house, and their deliverer.</li>
       <li>Released from her chains, the girl comes forward, the prize and the cause of his efforts.</li>
@@ -94,7 +108,8 @@
       <li value="745"> acquiring a new rigidity in branches and fronds.</li>
       <li>And the ocean nymphs try out this wonder on more plants, and are delighted that the same thing happens at its touch, and repeat it by scattering the seeds from the plants through the waves.</li>
       <li value="750">Even now corals have the same nature, hardening at a touch of air, and what was alive, under the water, above water is turned to stone.</li>
-      <li>To the three gods, he builds the same number of altars out of turf, to you Mercury on the left, to you Minerva, warlike virgin, on the right,</li>
+
+      <li class="top-space">To the three gods, he builds the same number of altars out of turf, to you Mercury on the left, to you Minerva, warlike virgin, on the right,</li>
       <li value="755"> and an altar of Jupiter in the centre.</li>
       <li>He sacrifices a cow to Minerva, a calf to the wing-footed god, and a bull to you, greatest of the gods.</li>
       <li>Then he claims Andromeda, without a dowry, valuing her as the worthiest prize.</li>
@@ -104,7 +119,7 @@
     </ol>
 
   <footer>
-    <p style="color: #df8a13;">Ovid, <cite>The Metamorphoses</cite>, Book 4, verses 663 to 764.</p>
+    <p style="color: #df8a13;">Ovid, <cite>The Metamorphoses</cite>, Book <abbr title="four">IV</abbr>, verses 663 to 764.</p>
   </footer>
 
   </article>
