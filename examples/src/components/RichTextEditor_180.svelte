37d01f1748f36c2b5b7fa9369a418e7e00e4d702
tidiview <francois.vidit@laposte.net>
2021-04-30 12:02:17 +0200

    fixed typos

--- examples/src/components/RichTextEditor_180.svelte
+++ examples/src/components/RichTextEditor_180.svelte
@@ -72,13 +72,14 @@
 </hgroup>
 
 <p><span title="Prénom">François</span>, comme prénom, se prononce: <span title="prononciation du prénom « François »">[fʁɑ̃.swɑ]</span></p>
-<p><audio controls="1" title="prononciation du prénom « François »" alt="" id="audio0"><source src="https://francois-vidit.com/user/sites/profile/pages/01.home/02.nom/Francois.mp3" type="audio/mp3">Your browser does not support the audio tag.</audio></p>
+
+<p><audio controls="1" title="prononciation du prénom « François »" alt="" id="audio0"><source src="https://francois-vidit.com/user/sites/profile/pages/01.home/02.nom/Francois.mp3" type="audio/mpeg">Votre navigateur ne prend pas en charge la balise audio.</audio></p>
 
 <p><span title="Nom">VIDIT</span>, ici le nom, se prononce: <span title="prononciation du nom de famille « VIDIT »">[vidi]</span></p>
-<tr>
-<p><audio controls="1" title="prononciation du nom de famille « VIDIT »" alt="" id="audio1"><source src="https://francois-vidit.com/user/sites/profile/pages/01.home/02.nom/VIDIT.mp3" type="audio/mp3">Your browser does not support the audio tag.</audio></p>
 
-<p><span style="color:#000e5f">François VIDIT est </span> <span title="Profession" style="color:#000e5f">guide-interprète japonais</span><span style="color:#000e5f"> à </span><span style="color:#000e5f">Paris</span><span style="color:#000e5f">. Depuis plus de <time datetime="P15Y">15 ans</time>, il exerce sous diverses formes, du guidage touristique, conseil interculturel, interprétariat consécutif, à la traduction.</span></p>
+<p><audio controls="1" title="prononciation du nom de famille « VIDIT »" alt="" id="audio1"><source src="https://francois-vidit.com/user/sites/profile/pages/01.home/02.nom/VIDIT.mp3" type="audio/mpeg">Votre navigateur ne prend pas en charge la balise audio.</audio></p>
+
+<p><span style="color:#000e5f">François VIDIT est </span> <span title="Profession" style="color:#000e5f">guide-interprète japonais</span><span style="color:#000e5f"> à </span><span style="color:#000e5f">Paris</span><span style="color:#000e5f">. Depuis plus de <time datetime="P20Y">20 ans</time>, il exerce sous diverses formes, du guidage touristique, conseil interculturel, interprétariat consécutif, à la traduction.</span></p>
 
 </div>
 </div>
@@ -94,12 +95,14 @@
 <p>Comme ses parents, François VIDIT est Parisien.</p>
 <p>Il découvre la culture japonaise grâce au <a href="https://fr.wikipedia.org/wiki/Go_(jeu)" title="https://fr.wikipedia.org/wiki/Go_(jeu)">jeu de go</a> et commence à en apprendre la langue, en autodidacte, à l'âge de dix-sept ans.</p>
 <p>Un diplôme de l'<a href="https://www.google.fr/maps/place/E.N.S.A.P.L.V : École Nationale Supérieure d'Architecture de Paris La Villette/@48.8936163,2.378878,17z/data=!3m1!4b1!4m5!3m4!1s0x47e66dd283e5884d:0x9d97e01a0a060349!8m2!3d48.8936128!4d2.3810667" title="https://www.google.fr/maps/place/E.N.S.A.P.L.V.">École Nationale d'Architecture de Paris-La Villette</a> en poche, il part au Japon.</p>
-<p>Il intègre une <a href="http://www.garde-intl.com/" title="http://www.garde-intl.com/">entreprise</a> de conception et de réalisation de réseaux commerciaux de marques de luxe dans les plus grandes villes japonaises dans le prestigieux quartier d'<a href="https://www.google.fr/maps/place/Minamiaoyama, Minato-ku, Tokyo 107-0062, Japon/@35.6645978,139.7148829,15.75z/data=!4m5!3m4!1s0x60188b6254cc464f:0x99773132a4b583e6!8m2!3d35.6653189!4d139.7149995" title="https://www.google.fr/maps/place/Minamiaoyama">Aoyama</a> à <a href="#fn:1" class="footnote-ref">Tōkiō</a>.</p>
+<p>Il intègre une <a href="http://www.garde-intl.com/" title="http://www.garde-intl.com/">entreprise</a> de conception et de réalisation de réseaux commerciaux de marques de luxe dans les plus grandes villes japonaises dans le prestigieux quartier d'<a href="https://www.google.fr/maps/place/Minamiaoyama, Minato-ku, Tokyo 107-0062, Japon/@35.6645978,139.7148829,15.75z/data=!4m5!3m4!1s0x60188b6254cc464f:0x99773132a4b583e6!8m2!3d35.6653189!4d139.7149995" title="https://www.google.fr/maps/place/Minamiaoyama">Aoyama</a> à <a href="#fn:1">Tōkiō</a>.</p>
 <p>De retour en France, il change de profession et devient interprète.</p>
 
 <aside>
-<p><i>Tōkiō</i>: cette orthographe est rare, mais c'est la transgraphie française régulière; elle date de la fin du dix-neuvième siècle; elle a encore cours officiellement, par exemple dans certains actes officiels dont on souhaite marquer le caractère solennel; à la différence de la transgraphie anglaise actuelle, elle reflète fidèlement la prononciation originale en indiquant les nuances de prononciation par accentuation; transgraphie anglaise: Tokyo.</p>
-<p class="puce"><a href="#fnref1:1"> retour au texte ↩</a></p>
+  <p id="fn:1"><i>Tōkiō</i>: cette orthographe est rare, mais c'est la transgraphie française régulière; elle date de la fin du dix-neuvième siècle; elle a encore cours officiellement, par exemple dans certains actes officiels dont on souhaite marquer le caractère solennel; à la différence de la transgraphie anglaise actuelle, elle reflète fidèlement la prononciation originale en indiquant les nuances de prononciation par accentuation; transgraphie anglaise: Tokyo.</p>
+  <footer>
+    <p class="puce"><a href="#fnref1:1">retour au texte ↩</a></p>
+  </footer>
 </aside>
 
 </div>
@@ -107,7 +110,9 @@
 
 <div class="pure-g about-div">
 <div id="about_guider" class="block pure-u-3-4">
+
 <h2>guider, c'est partager!</h2>
+
 <p>Se confronter à des visages différents, éprouver le caractère solennel de la rencontre, se trouver en dehors des habitudes, et par là sentir se manifester une humanité nouvelle en soi-même, intimement et mystérieusement.</p>
 <p>Par là, avoir la chance de se trouver sans cesse défait de nombre de préjugés, plus universel à soi-même à la rencontre de l'humanité et du monde.</p>
 </div>
@@ -187,9 +192,10 @@
     <span class="year-width"><time datetime="1999">1999</time>:</span>&nbspObtention du Certificat de Compétence en Langue Japonaise <a href="http://www.jlpt.jp/e/index.html" title="http://www.jlpt.jp/e/index.html">Niveau 3</a> de l'<a id="fnref1:1" href="#fn:1">Association of International Education, Japan</a>
   </li>
   <li>
-    <span class="year-width"><time datetime="2003">2003</time>:</span>&nbspAdmis au <abbr title="huitième">8<suli>ème</suli></abbr> <a href="http://www.kanken.or.jp/bjt/english/" title="http://www.kanken.or.jp/bjt/english/">Test de compétence en japonais des affaires</a> <a id="fnref1:2" href="#fn:2"><abbr title="Japanese Listening and Reading Test">JLRT</abbr></a> <a id="fnref1:3" href="#fn:3"><abbr title="Japanese External Trade Organization">JETRO</abbr></a>
+    <span class="year-width"><time datetime="2003">2003</time>:</span>&nbspAdmis au <abbr title="huitième">8<sup>ème</sup></abbr> <a href="http://www.kanken.or.jp/bjt/english/" title="http://www.kanken.or.jp/bjt/english/">Test de compétence en japonais des affaires</a> <a id="fnref1:2" href="#fn:2"><abbr title="Japanese Listening and Reading Test">JLRT</abbr></a> <a id="fnref1:3" href="#fn:3"><abbr title="Japanese External Trade Organization">JETRO</abbr></a>
   </li>
-  <li><span class="year-width"><time datetime="2011">2011</time>:</span>&nbspObtention du titre de <a href="http://www.entreprises.gouv.fr/tourisme/metiers/guide-conferencier" title="http://www.entreprises.gouv.fr/tourisme/metiers/guide-conferencier">Guide-Interprète National</a> à l'<a href="https://www.google.fr/maps/place/Université Toulouse - Jean Jaurès/@43.5779941,1.3996184,17z/data=!3m1!4b1!4m5!3m4!1s0x12aeba4c48abb62d:0xaa534804f879cf3b!8m2!3d43.5779941!4d1.4018071" title="https://www.google.fr/maps/place/Université+Toulouse+-+Jean+Jaurès">Université Toulouse-Jean Jaurès</a>
+  <li>
+    <span class="year-width"><time datetime="2011">2011</time>:</span>&nbspObtention du titre de <a href="http://www.entreprises.gouv.fr/tourisme/metiers/guide-conferencier" title="http://www.entreprises.gouv.fr/tourisme/metiers/guide-conferencier">Guide-Interprète National</a> à l'<a href="https://www.google.fr/maps/place/Université Toulouse - Jean Jaurès/@43.5779941,1.3996184,17z/data=!3m1!4b1!4m5!3m4!1s0x12aeba4c48abb62d:0xaa534804f879cf3b!8m2!3d43.5779941!4d1.4018071" title="https://www.google.fr/maps/place/Université+Toulouse+-+Jean+Jaurès">Université Toulouse-Jean Jaurès</a>
   </li>
 </ol>
 
@@ -204,7 +210,7 @@
 
 <h2>informations pratiques</h2>
 
-<p><span style="min-width: 260px;"><abbr title="numéro">n°</abbr> d'imma<wbr>tri<wbr>cu<wbr>lation profes<wbr>sion<wbr>nelle: </span><wbr><samp>GC1375538P</samp></p>
+<p><span style="min-width: 260px;;float: left;"><abbr title="numéro">n°</abbr> d'imma<wbr>tri<wbr>cu<wbr>lation profes<wbr>sion<wbr>nelle: </span><wbr><samp>GC1375538P</samp></p>
 <p><span style="min-width: 260px;float: left;">affi<wbr>liation: </span><wbr><a id="fnref1:4" href="http://www.agij-paris.com/" title="http://www.agij-paris.com/"><abbr title="Association des Guide-Interprètes de langue Japonaise">AGIJ</abbr></a></p>
 <p><span style="width: 260px;float: left;">autres lan<wbr>gues parlées: </span><wbr>anglais, espagnol</p>
 <p><span style="width: 260px;float: left;">aire d'exer<wbr>cice: </span><wbr><a href="https://www.google.fr/maps/place/France/@46.1279326,-2.2764268,6z/data=!3m1!4b1!4m5!3m4!1s0xd60a02933785731:0x6bfd3f96c747d9f7!8m2!3d46.227638!4d2.213749" title="https://www.google.fr/maps/place/France/">France</a>, <a href="https://www.google.fr/maps/place/Bruges, Belgique/@51.2605832,3.0820626,11z/data=!3m1!4b1!4m5!3m4!1s0x47c350d0c11e420d:0x1aa2f35ac8834df7!8m2!3d51.209348!4d3.2246995" title="https://www.google.fr/maps/place/Bruges,+Belgique/">Bruges</a></p>
