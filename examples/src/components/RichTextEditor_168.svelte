7d813162106301cf817201b258ae103d8fae7558
tidiview <francois.vidit@laposte.net>
2021-05-04 20:09:14 +0200

    added other refs lang

--- examples/src/components/RichTextEditor_168.svelte
+++ examples/src/components/RichTextEditor_168.svelte
@@ -4,7 +4,7 @@
   import ProsemirrorEditor from "../../../ProsemirrorEditor.svelte";
   import { createExtendedThreeRichTextEditor, clear, toHTML, toPlainText, toJSON } from "../../../state";
 
-  const html = `
+  const html = `<!-- EN:168 JA:135  -->
 <article>
 
 <hgroup>
