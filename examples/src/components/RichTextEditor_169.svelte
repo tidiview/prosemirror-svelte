d666b734b25ec2e7898369bf580b38aa05ecf3f9
tidiview <francois.vidit@laposte.net>
2021-05-04 18:06:40 +0200

    added refs to other langs

--- examples/src/components/RichTextEditor_169.svelte
+++ examples/src/components/RichTextEditor_169.svelte
@@ -4,7 +4,7 @@
   import ProsemirrorEditor from "../../../ProsemirrorEditor.svelte";
   import { createExtendedThreeRichTextEditor, clear, toHTML, toPlainText, toJSON } from "../../../state";
 
-  const html = `
+  const html = `<!-- EN:200 JA:137  -->
 <article>
 
 <hgroup>
