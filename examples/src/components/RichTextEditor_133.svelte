6c6b07a842c03c7a8d8235c9b6685f5551c90546
tidiview <francois.vidit@laposte.net>
2021-05-05 22:11:21 +0200

    fixed typo

--- examples/src/components/RichTextEditor_133.svelte
+++ examples/src/components/RichTextEditor_133.svelte
@@ -4,7 +4,7 @@
   import ProsemirrorEditor from "../../../ProsemirrorEditor.svelte";
   import { createExtendedThreeRichTextEditor, clear, toHTML, toPlainText, toJSON } from "../../../state";
 
-  const html = `<!-- FR:166 EN:197  -->
+  const html = `<!-- FR:165 EN:197  -->
 
 <article>
 
