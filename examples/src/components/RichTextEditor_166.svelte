df4988dbc1e5dd1d7032fb10a1df69031f80a6eb
tidiview <francois.vidit@laposte.net>
2021-05-05 14:14:08 +0200

    added refs to other langs

--- examples/src/components/RichTextEditor_166.svelte
+++ examples/src/components/RichTextEditor_166.svelte
@@ -4,7 +4,7 @@
   import ProsemirrorEditor from "../../../ProsemirrorEditor.svelte";
   import { createExtendedThreeRichTextEditor, clear, toHTML, toPlainText, toJSON } from "../../../state";
 
-  const html = `
+  const html = `<!-- JA:134 EN:197  -->
 <article>
 
 <h1 class="green">Marly selon Pierre-Denis MARTIN</h1>
