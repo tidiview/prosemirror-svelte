4d7fc212be153aee75a0a92d970523e29c830d05
tidiview <francois.vidit@laposte.net>
2021-05-08 15:11:46 +0200

    added <time>, <abbr>, note <aside, ref to other lang

--- examples/src/components/RichTextEditor_157.svelte
+++ examples/src/components/RichTextEditor_157.svelte
@@ -4,7 +4,7 @@
   import ProsemirrorEditor from "../../../ProsemirrorEditor.svelte";
   import { createExtendedThreeRichTextEditor, clear, toHTML, toPlainText, toJSON } from "../../../state";
 
-  const html = `
+  const html = `<!-- JA:125 EN:188  -->
 <article>
 
 <h1 class="green">La Joconde selon Théophile Gautier</h1>
@@ -32,11 +32,11 @@
 <div class="notices green">
 
 <p>La description de <a href="https://fr.wikipedia.org/wiki/La_Joconde" title="https://fr.wikipedia.org/wiki/La_Joconde">la Joconde</a> que nous présentons ci-dessous a été écrite par l'écrivain romantique <a href="https://fr.wikipedia.org/wiki/Théophile_Gautier" title="https://fr.wikipedia.org/wiki/Théophile_Gautier">Théophile GAUTIER</a> et publiée à Paris en <time datetime="1867">1867</time>.</p>
-<p>C'est une description capitale pour l'histoire du tableau car elle va à la fois mythifiée et détournée l'œuvre de <a href="https://fr.wikipedia.org/wiki/Léonard_de_Vinci" title="https://fr.wikipedia.org/wiki/Léonard_de_Vinci">Léonard de Vinci</a> au nom du mystère qu'elle paraît incarner et influencer une bonne partie des regards biaisés portés sur l'œuvre au cours du <abbr title="vingtième">XX<sup>ème</sup></abbr> siècle.</p>
+<p>C'est une description capitale pour l'histoire du tableau car elle va à la fois mythifiée et détournée l'œuvre de <a href="https://fr.wikipedia.org/wiki/Léonard_de_Vinci" title="https://fr.wikipedia.org/wiki/Léonard_de_Vinci">Léonard de Vinci</a> au nom du mystère qu'elle paraît incarner et influencer une bonne partie des regards biaisés portés sur l'œuvre au cours du <time datetime="1950"><abbr title="vingtième">XX<sup>ème</sup></abbr> siècle</time>.</p>
 <p>Nous pensons que si cette description est misogyne, que cette misogynie ne fait partie ni de l'œuvre de Léonard de Vinci encore moins de l'émotion artistique et que la Joconde gagnerait à être débarassée des chimères de Théophile Gautier.</p>
 </div>
 
-<h2 class="green">La Joconde selon Théophile Gautier</h2>
+<h2 class="green">La Joconde selon le texte de Théophile Gautier extrait du <cite>guide par les principaux écrivains et artistes de France</cite></h2>
 
 <p>Notre admiration et notre amour pour cette divine Monna Lisa del Giocondo ne datent pas d'hier, et bien des passions pour des êtres réels ont duré moins longtemps.</p>
 <p>Il y a une douzaine d'années que nous écrivions ces lignes un peu trop enthousiastes peut-être, mais qui rendent fidèlement notre impression:</p>
@@ -57,7 +57,7 @@ il n'en aurait tracé qu'un, et les ailes de son désir eussent refusé de le po
 <p>Nous l'avons revue depuis bien des fois, cette adorable Joconde, et notre déclaration d'amour ne nous paraît pas trop brûlante.</p>
 <p>Elle est toujours là, souriant avec une moqueuse volupté à ses innombrables amants.</p>
 <p><span style="color: #3d8b3d;">Sur son front repose cette sérénité d'une femme sûre d'être éternellement belle, et qui se sent supérieure à l'idéal de tous les poètes et de tous les artistes.</span></p>
-<p>Le divin Léonard mit quatre ans à faire ce portrait, qu'il ne pouvait se décider à quitter, et qu'il ne considéra jamais comme fini;
+<p>Le divin Léonard mit <time datetime="P4Y">quatre ans</time> à faire ce portrait, qu'il ne pouvait se décider à quitter, et qu'il ne considéra jamais comme fini;
 pendant les séances, des musiciens exécutaient des morceaux pour égayer le beau modèle et empêcher ses traits charmants de prendre un air d'ennui ou de fatigue.</p>
 <p>Doit-on regretter que le noir particulier qu'employait Léonard, et dont il était l'inventeur, ait prévalu dans les teintes de la Monna Lisa et leur ait donné cette délicieuse harmonie violâtre?
 Cette tonalité abstraite qui est comme le coloris de l'idéal?
@@ -65,8 +65,12 @@ Nous ne le pensons pas.</p>
 <p>Maintenant, le mystère s'ajoute au charme, et le tableau, dans sa fraîcheur, était peut-être moins séduisant.</p>
 
 <footer>
-  <p><span style="color: #3d8b3d;"><a href="https://fr.wikipedia.org/wiki/Théophile_Gautier" title="https://fr.wikipedia.org/wiki/Théophile_Gautier" class="green">Théophile Gautier</a>, extrait d'un guide ouvrage collectif destiné aux visiteurs de l'Exposition universelle, Paris, par les principaux écrivains et artistes de France</span>, auquel participaient également <a href="https://fr.wikipedia.org/wiki/Alexandre_Dumas_fils" title="https://fr.wikipedia.org/wiki/Alexandre_Dumas_fils">Dumas</a>, <a href="https://fr.wikipedia.org/wiki/Charles-Augustin_Sainte-Beuve" title="https://fr.wikipedia.org/wiki/Charles-Augustin_Sainte-Beuve">Sainte-Beuve</a>, <a href="https://fr.wikipedia.org/wiki/Eugène_Viollet-le-Ducク" title="https://fr.wikipedia.org/wiki/Eugène_Viollet-le-Ducク">Viollet-le-Duc</a>, <a href="https://fr.wikipedia.org/wiki/George_Sand" title="https://fr.wikipedia.org/wiki/George_Sand">George Sand</a>, <a href="https://fr.wikipedia.org/wiki/Nadar" title="https://fr.wikipedia.org/wiki/Nadar">Nadar</a>… puis réédité comme livre à la mort de l'auteur sous le titre «Guide de l'amateur au musée du Louvre».</p>
+  <p>Théophile Gautier, <cite>Le Musée du Louvre</cite>, texte extrait du <cite>Paris Guide par les principaux écrivains et artistes de la France</cite></p>
 </footer>
+
+<aside>
+  <p><span style="color: #3d8b3d;"><a href="https://fr.wikipedia.org/wiki/Théophile_Gautier" title="https://fr.wikipedia.org/wiki/Théophile_Gautier" class="green">Théophile Gautier</a>, extrait du <cite>guide par les principaux écrivains et artistes de France</cite> ouvrage collectif destiné aux visiteurs de l'Exposition universelle de <time datetime="1867">1867</time> à Paris, </span>, auquel participaient également <a href="https://fr.wikipedia.org/wiki/Alexandre_Dumas_fils" title="https://fr.wikipedia.org/wiki/Alexandre_Dumas_fils">Dumas</a>, <a href="https://fr.wikipedia.org/wiki/Charles-Augustin_Sainte-Beuve" title="https://fr.wikipedia.org/wiki/Charles-Augustin_Sainte-Beuve">Sainte-Beuve</a>, <a href="https://fr.wikipedia.org/wiki/Eugène_Viollet-le-Duc" title="https://fr.wikipedia.org/wiki/Eugène_Viollet-le-Duc">Viollet-le-Duc</a>, <a href="https://fr.wikipedia.org/wiki/George_Sand" title="https://fr.wikipedia.org/wiki/George_Sand">George Sand</a>, <a href="https://fr.wikipedia.org/wiki/Nadar" title="https://fr.wikipedia.org/wiki/Nadar">Nadar</a>… puis réédité comme livre à la mort de l'auteur sous le titre «Guide de l'amateur au musée du Louvre».</p>
+</aside>
 
 </article>
 `;
