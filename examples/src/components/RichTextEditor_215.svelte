fedbc3de7b7a9e0fa8a1d92913eb3e7ab4b10011
tidiview <francois.vidit@laposte.net>
2021-05-13 13:07:22 +0200

    added width, height, loading, decoding to <img>, ref to other lang

--- examples/src/components/RichTextEditor_215.svelte
+++ examples/src/components/RichTextEditor_215.svelte
@@ -4,8 +4,7 @@
   import ProsemirrorEditor from "../../../ProsemirrorEditor.svelte";
   import { createExtendedThreeRichTextEditor, clear, toHTML, toPlainText, toJSON } from "../../../state";
 
-  const html = `
-<!-- Page 17 -->
+  const html = `<!-- Page 17 JA:220 FR:225 -->
 
 <article>
 
@@ -23,7 +22,7 @@
         https://francois-vidit.com/user/sites/docs/pages/01.home/06.bruges/01.hopital-saint-jean/01.saint-jean/03.saint-jean_3/barbe-la-grande-martyre-840.webp 840w,
         https://francois-vidit.com/user/sites/docs/pages/01.home/06.bruges/01.hopital-saint-jean/01.saint-jean/03.saint-jean_3/barbe-la-grande-martyre-1280.webp 1280w,
         https://francois-vidit.com/user/sites/docs/pages/01.home/06.bruges/01.hopital-saint-jean/01.saint-jean/03.saint-jean_3/barbe-la-grande-martyre-1600.webp 1600w,
-        https://francois-vidit.com/user/sites/docs/pages/01.home/06.bruges/01.hopital-saint-jean/01.saint-jean/03.saint-jean_3/barbe-la-grande-martyre-1920.webp 1920w" type="image/webp"><img src="https://francois-vidit.com/user/sites/docs/pages/01.home/06.bruges/01.hopital-saint-jean/01.saint-jean/03.saint-jean_3/barbe-la-grande-martyre-640.jpg" title="Hans MEMLING, "Triptych of John the Baptist and John the Evangelist", detail of the central panel, Catherine of Alexandria, Hans Memling Museum, Old St John's Hospital, Bruges" alt="Hans MEMLING, "Triptych of John the Baptist and John the Evangelist", detail of the central panel, Catherine of Alexandria, Hans Memling Museum, Old St John's Hospital, Bruges" class="class-40-img" sizes="(max-width: 767px) 98vw, (min-width: 959px) 50vw, 86vw" srcset="
+        https://francois-vidit.com/user/sites/docs/pages/01.home/06.bruges/01.hopital-saint-jean/01.saint-jean/03.saint-jean_3/barbe-la-grande-martyre-1920.webp 1920w" type="image/webp"><img src="https://francois-vidit.com/user/sites/docs/pages/01.home/06.bruges/01.hopital-saint-jean/01.saint-jean/03.saint-jean_3/barbe-la-grande-martyre-640.jpg" title="Hans MEMLING, "Triptych of John the Baptist and John the Evangelist", detail of the central panel, Catherine of Alexandria, Hans Memling Museum, Old St John's Hospital, Bruges" alt="Hans MEMLING, "Triptych of John the Baptist and John the Evangelist", detail of the central panel, Catherine of Alexandria, Hans Memling Museum, Old St John's Hospital, Bruges" class="class-40-img" width="640" height="1138" loading="eager" decoding="async" sizes="(max-width: 767px) 98vw, (min-width: 959px) 50vw, 86vw" srcset="
         https://francois-vidit.com/user/sites/docs/pages/01.home/06.bruges/01.hopital-saint-jean/01.saint-jean/03.saint-jean_3/barbe-la-grande-martyre-280.jpg 280w,
         https://francois-vidit.com/user/sites/docs/pages/01.home/06.bruges/01.hopital-saint-jean/01.saint-jean/03.saint-jean_3/barbe-la-grande-martyre-380.jpg 380w,
         https://francois-vidit.com/user/sites/docs/pages/01.home/06.bruges/01.hopital-saint-jean/01.saint-jean/03.saint-jean_3/barbe-la-grande-martyre-480.jpg 480w,
