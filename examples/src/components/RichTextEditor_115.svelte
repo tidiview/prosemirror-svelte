060546f77fd66d169effbdaf867f442c95198ed1
tidiview <francois.vidit@laposte.net>
2021-05-13 17:39:22 +0200

    added <article>, <footer>, <time>, <p>, p.conversation, replace <em> <strong> by <i> <b>

--- examples/src/components/RichTextEditor_115.svelte
+++ examples/src/components/RichTextEditor_115.svelte
@@ -1,53 +1,50 @@
 <script>
   import { onMount } from "svelte";
 
-  import ProsemirrorEditor from "../../../ProsemirrorEditor.svelte";
+  import ProsiirrorEditor from "../../../ProsemirrorEditor.svelte";
   import { createExtendedThreeRichTextEditor, clear, toHTML, toPlainText, toJSON } from "../../../state";
 
-  const html = `
-<p><span>２３</span>日<i class="fa fa-calendar-o fa-2x"></i></p>
-<p>７月</p>
-<p><span>２０１６年</span></p>
-<h1 class="yellow"><a href="/blog/ja/bokusi" rel="bookmark">すばらしい美男美女</a></h1>
-<div class="notices yellow">
-<p>中国文学者 <a href="https://ja.wikipedia.org/wiki/貝塚茂樹" title="https://ja.wikipedia.org/wiki/貝塚茂樹"><ruby>貝<rp>(</rp><rt>かい</rt><rp>)</rp>塚<rp>(</rp><rt>づか</rt><rp>)</rp>茂<rp>(</rp><rt>しげ</rt><rp>)</rp>樹<rp>(</rp><rt>き</rt><rp>)</rp></ruby></a> が書いた「 <a href="https://www.amazon.co.jp/諸子百家――中国古代の思想家たち-岩波新書-貝塚-茂樹/dp/4004130476/378-6956610-7563751?ie=UTF8&%2AVersion%2A=1&%2Aentries%2A=0" title="https://www.amazon.co.jp/諸子百家――中国古代の思想家たち-岩波新書-貝塚-茂樹/dp/4004130476/378-6956610-7563751?ie=UTF8&*Version*=1&*entries*=0"><ruby>諸<rp>(</rp><rt>しょ</rt><rp>)</rp>子<rp>(</rp><rt>し</rt><rp>)</rp>百家<rp>(</rp><rt>ひゃっか</rt><rp>)</rp></ruby></a> 」を読んで、初めて、 <a href="https://ja.wikipedia.org/wiki/墨子" title="https://ja.wikipedia.org/wiki/墨子"><ruby>墨<rp>(</rp><rt>ぼく</rt><rp>)</rp>子<rp>(</rp><rt>し</rt><rp>)</rp></ruby></a> を知りました。面白い！</p></div>
+  const html = `<!-- FR:NONE EN:NONE -->
 
-<h2 class="yellow"><ruby>墨<rp>(</rp><rt>ぼく</rt><rp>)</rp>子<rp>(</rp><rt>し</rt><rp>)</rp></ruby></h2>
+<article>
 
-<dl>
-<dt>「現代天下の識者諸氏は平素の言論でいつも有能の士を任用せよといわれる。<br>
-一度大衆にたいして政令を下すだんになると、有能の士を採用してその能力を発揮させようとはしない。<br>
-これは天下の識者が小問題にはよく気代くが、大問題を忘れている証拠である。」</dd>
-</dl>
-<dl>
-<dt>どうして小問題にはよく気付くが、大問題を忘れるようになったのでしょうか。</dt>
-<dd>
-「現代の諸国の<strong>国王・大臣・有力者</strong>らは一頭の牛、半でも、自分の手で屠殺できないで、きっとよい居殺者をさがす。
-一着分の反物でも自分で仕立てないで、きっとよい仕立屋をさがす。
-<strong>国王・大臣・有力者</strong>はこういうことにかけては、<em>骨肉の親戚、手柄もない貴族富豪、すばらしい美男美女</em>でもその能力がかけているのを知っていれば使おうともしない。
-なぜかといえば材料が無駄になることを恐れるからだ。<strong>国王・大臣・有力者</strong>はこのことにかけては有能の士を採用して能力を発揮させずにはいうれない。
-以下同様にして、一頭の病馬も良獣医、ひと張りの強弓師にまかせる。・・・・・それなのにことが一旦国家に関すると、事情ががらりとかわる。
-<strong>国王・大臣・有力者</strong>は<em>骨肉の親戚、手柄もない貴族富豪、すばらしい美男美女</em>を登用する。
-<strong>国王・大臣・有力者</strong>の国家にたいする気のいれ方は、一つの強弓、病馬、衣服、牛羊の資財にも及ばない。
-自分はこの理由によって天下の有識者がみんな小問題に気がついて大問題を忘れていることを知るのである。これはたとえば唖を外交官に任命し、聾を音楽師に雇うようなものではないか。」</dd>
-<dt>という調子で議論が進められる。そして、</dt>
-<dd>
-「それだから古代聖徳たかい王が天下を治められたとき、富をあさえ、位をあたえたのは、<strong>国王・大臣・有力者</strong>たちの<em>骨肉の親戚、手柄もない貴族富豪、すばらしい美男美女</em>ばかりではなかったのだ。」</dd>
-<dt>といって、<ruby>堯<rp>(</rp><rt>ぎょう</rt><rp>)</rp></ruby>帝が無名の舜を後嗣にえらび、湯王か名相<ruby>伊<rp>(</rp><rt>い</rt><rp>)</rp>尹<rp>(</rp><rt>いん</rt><rp>)</rp></ruby>を板前から抜握した故事などを述べて、歴史的に「尚賢」がいかに有効な政策であるかを実証しようとする。</dt>
-<dd>
-「現今の天下の士君子諸君はだれも富貴を欲して貧賤を嫌うだろう。」</dd>
-<dt>その通りです。</dt>
-<dd>
-「はどうすれば富貴をえて貧賤からのがれられると思うか。もちろん賢つまり有能の士なること以外にないと考えるだろうね。<br>
-それならその有能の士になるにはどうすればよいのですか」</dd>
-</dl>
+<p><time datetime="2016-07-23">２０１６年７月２３日</time></p>
+
+<h1 class="yellow">すばらしい美男美女</h1>
+
 <div class="notices yellow">
-<dl>
-<dt>引用:</dt>
-<dd>
-<a href="https://ja.wikipedia.org/wiki/貝塚茂樹" title="https://ja.wikipedia.org/wiki/貝塚茂樹"><ruby>貝<rp>(</rp><rt>かい</rt><rp>)</rp>塚<rp>(</rp><rt>づか</rt><rp>)</rp>茂<rp>(</rp><rt>しげ</rt><rp>)</rp>樹<rp>(</rp><rt>き</rt><rp>)</rp></ruby></a>、「<a href="https://www.amazon.co.jp/諸子百家――中国古代の思想家たち-岩波新書-貝塚-茂樹/dp/4004130476/378-6956610-7563751?ie=UTF8&%2AVersion%2A=1&%2Aentries%2A=0" title="https://www.amazon.co.jp/諸子百家――中国古代の思想家たち-岩波新書-貝塚-茂樹/dp/4004130476/378-6956610-7563751?ie=UTF8&*Version*=1&*entries*=0"><ruby>諸<rp>(</rp><rt>しょ</rt><rp>)</rp>子<rp>(</rp><rt>し</rt><rp>)</rp>百家<rp>(</rp><rt>ひゃっか</rt><rp>)</rp></ruby></a>、中国古代の思想家たち」、岩波新書４３７（１９７５年１２月２０日第１９刷発行）<br>
-第二章「人類愛と平和についての対話、<a href="https://ja.wikipedia.org/wiki/墨子" title="https://ja.wikipedia.org/wiki/墨子">墨子</a>の思想」、第４０ページーから</dd>
-</dl>
+<p>中国文学者 <a href="https://ja.wikipedia.org/wiki/貝塚茂樹" title="https://ja.wikipedia.org/wiki/貝塚茂樹"><ruby>貝<rp>（</rp><rt>かい</rt><rp>）</rp>塚<rp>（</rp><rt>づか</rt><rp>）</rp>茂<rp>（</rp><rt>しげ</rt><rp>）</rp>樹<rp>（</rp><rt>き</rt><rp>）</rp></ruby></a> が書いた「 <a href="https://www.amazon.co.jp/諸子百家――中国古代の思想家たち-岩波新書-貝塚-茂樹/dp/4004130476/378-6956610-7563751?ie=UTF8&%2AVersion%2A=1&%2Aentries%2A=0" title="https://www.amazon.co.jp/諸子百家――中国古代の思想家たち-岩波新書-貝塚-茂樹/dp/4004130476/378-6956610-7563751?ie=UTF8&*Version*=1&*entries*=0"><cite><ruby>諸<rp>（</rp><rt>しょ</rt><rp>）</rp>子<rp>（</rp><rt>し</rt><rp>）</rp>百家<rp>（</rp><rt>ひゃっか</rt><rp>）</rp></ruby></cite></a> 」を読んで、初めて、 <a href="https://ja.wikipedia.org/wiki/墨子" title="https://ja.wikipedia.org/wiki/墨子"><ruby>墨<rp>（</rp><rt>ぼく</rt><rp>）</rp>子<rp>（</rp><rt>し</rt><rp>）</rp></ruby></a> を知りました。面白い！</p></div>
+
+<h2 class="yellow"><ruby>墨<rp>（</rp><rt>ぼく</rt><rp>）</rp>子<rp>（</rp><rt>し</rt><rp>）</rp></ruby></h2>
+
+<p class="conversation">「現代天下の識者諸氏は平素の言論でいつも有能の士を任用せよといわれる。</p>
+<p class="conversation">一度大衆にたいして政令を下すだんになると、有能の士を採用してその能力を発揮させようとはしない。</p>
+<p class="conversation">これは天下の識者が小問題にはよく気代くが、大問題を忘れている証拠である。」</p>
+<p>どうして小問題にはよく気付くが、大問題を忘れるようになったのでしょうか。</p>
+<p class="conversation">「現代の諸国の<b>国王・大臣・有力者</b>らは一頭の牛、半でも、自分の手で屠殺できないで、きっとよい居殺者をさがす。</p>
+<p class="conversation">一着分の反物でも自分で仕立てないで、きっとよい仕立屋をさがす。</p>
+<p class="conversation"><b>国王・大臣・有力者</b>はこういうことにかけては、<i>骨肉の親戚、手柄もない貴族富豪、すばらしい美男美女</i>でもその能力がかけているのを知っていれば使おうともしない。</p>
+<p class="conversation">なぜかといえば材料が無駄になることを恐れるからだ。<b>国王・大臣・有力者</b>はこのことにかけては有能の士を採用して能力を発揮させずにはいうれない。</p>
+<p class="conversation">以下同様にして、一頭の病馬も良獣医、ひと張りの強弓師にまかせる。・・・・・それなのにことが一旦国家に関すると、事情ががらりとかわる。</p>
+<p class="conversation"><b>国王・大臣・有力者</b>は<i>骨肉の親戚、手柄もない貴族富豪、すばらしい美男美女</i>を登用する。</p>
+<p class="conversation"><b>国王・大臣・有力者</b>の国家にたいする気のいれ方は、一つの強弓、病馬、衣服、牛羊の資財にも及ばない。</p>
+<p class="conversation">自分はこの理由によって天下の有識者がみんな小問題に気がついて大問題を忘れていることを知るのである。これはたとえば唖を外交官に任命し、聾を音楽師に雇うようなものではないか。」という調子で議論が進められる。</p>
+<p>そして、</p>
+<p class="conversation">「それだから古代聖徳たかい王が天下を治められたとき、富をあさえ、位をあたえたのは、<b>国王・大臣・有力者</b>たちの<i>骨肉の親戚、手柄もない貴族富豪、すばらしい美男美女</i>ばかりではなかったのだ。」といって、</p>
+<p><ruby>堯<rp>（</rp><rt>ぎょう</rt><rp>）</rp></ruby>帝が無名の舜を後嗣にえらび、湯王か名相<ruby>伊<rp>（</rp><rt>い</rt><rp>）</rp>尹<rp>（</rp><rt>いん</rt><rp>）</rp></ruby>を板前から抜握した故事などを述べて、歴史的に「<a href="https://kotobank.jp/word/尚賢思想-1174687" title="https://kotobank.jp/word/尚賢思想-1174687"><ruby>尚<rp>（</rp><rt>しょう</rt><rp>）</rp>賢<rp>（</rp><rt>けん</rt><rp>）</rp></ruby></a>」がいかに有効な政策であるかを実証しようとする。</p>
+<p class="conversation">「現今の天下の士君子諸君はだれも富貴を欲して貧賤を嫌うだろう。」</p>
+<p>その通りです。</p>
+<p class="conversation">「はどうすれば富貴をえて貧賤からのがれられると思うか。もちろん賢つまり有能の士なること以外にないと考えるだろうね。</p>
+<p class="conversation">それならその有能の士になるにはどうすればよいのですか」</p>
+
+<footer>
+
+<div class="notices yellow">
+<p><a href="https://ja.wikipedia.org/wiki/貝塚茂樹" title="https://ja.wikipedia.org/wiki/貝塚茂樹"><ruby>貝<rp>（</rp><rt>かい</rt><rp>）</rp>塚<rp>（</rp><rt>づか</rt><rp>）</rp>茂<rp>（</rp><rt>しげ</rt><rp>）</rp>樹<rp>（</rp><rt>き</rt><rp>）</rp></ruby></a>、「<a href="https://www.amazon.co.jp/諸子百家――中国古代の思想家たち-岩波新書-貝塚-茂樹/dp/4004130476/378-6956610-7563751?ie=UTF8&%2AVersion%2A=1&%2Aentries%2A=0" title="https://www.amazon.co.jp/諸子百家――中国古代の思想家たち-岩波新書-貝塚-茂樹/dp/4004130476/378-6956610-7563751?ie=UTF8&*Version*=1&*entries*=0"><cite><ruby>諸<rp>（</rp><rt>しょ</rt><rp>）</rp>子<rp>（</rp><rt>し</rt><rp>）</rp>百家<rp>（</rp><rt>ひゃっか</rt><rp>）</rp></ruby></a>、中国古代の思想家たち</cite>」、岩波新書４３７、<time datetime="1975-12-20">１９７５年１２月２０日</time>第１９刷発行。第二章「人類愛と平和についての対話、墨子の思想」、第４０項から。</p>
+
+</footer>
+
+<article>
 `;
 
 
@@ -81,7 +78,7 @@
 
 </script>
 
-<ProsemirrorEditor
+<ProsiirrorEditor
   {editorState}
   bind:focus={focusEditor}
   on:change={handleChange}
