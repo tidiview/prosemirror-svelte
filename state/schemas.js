f9a0aac9a195d60a0fb0fd5af16827394fe17b60
tidiview <francois.vidit@laposte.net>
2021-05-14 11:17:25 +0200

    added space

--- state/schemas.js
+++ state/schemas.js
@@ -693,7 +693,7 @@ export const ExtendedThreerichTextSchema = new Schema({
           decoding: dom.getAttribute("decoding"),
         }
       }}],
-      toDOM(node) { let {classes, src, title, alt, usemap, sizes, srcset, id, height, width, loading, decoding} = node.attrs; return ["img", {"class": classes, src, title, alt, usemap, sizes, srcset, id, height, width, loading, decoding, "style": "background-size: cover;background: url('data:image/svg+xml;utf8,<svg xmlns=\"https://www.w3.org/2000/svg\" width=\"" + width +"\" height=\"" + height +"\" viewBox=\"0 0 " + width + "\ " + height + "\"><rect fill=\"#ddd\" width=\"" + width + "\" height=\"" + height + "\"/><text fill=\"rgba(0,0,0,0.5)\" font-family=\"sans-serif\" font-size=\"30\" dy=\"10.5\" font-weight=\"bold\" x=\"50%\" y=\"50%\" text-anchor=\"middle\">" + width +"×" + height +"</text></svg>') center center no-repeat;"}] },
+      toDOM(node) { let {classes, src, title, alt, usemap, sizes, srcset, id, height, width, loading, decoding} = node.attrs; return ["img", {"class": classes, src, title, alt, usemap, sizes, srcset, id, height, width, loading, decoding, "style": "background-size: cover;background: url('data:image/svg+xml;utf8,<svg xmlns=\"https://www.w3.org/2000/svg\" width=\"" + width +"\" height=\"" + height +"\" viewBox=\"0 0 " + width + "\ " + height + "\"><rect fill=\"#ddd\" width=\"" + width + "\" height=\"" + height + "\"/><text fill=\"rgba(0,0,0,0.5)\" font-family=\"sans-serif\" font-size=\"30\" dy=\"10.5\" font-weight=\"bold\" x=\"50%\" y=\"50%\" text-anchor=\"middle\">" + width +"×" + height + "</text></svg>') center center no-repeat;"}] },
     },
     figcaption: {
       content: "inline*",
