f02e375406c7d40de0b072155c2407bae3b3ec42
tidiview <francois.vidit@laposte.net>
2021-06-05 00:10:45 +0200

    added missing part of information on <ruby>

--- examples/src/components/RichTextEditor_111.svelte
+++ examples/src/components/RichTextEditor_111.svelte
@@ -63,59 +63,53 @@
 </article>
 `;
 
-  
+let focusEditor;
+let showEditorState = true;
+let editorState = createExtendedThreeRichTextEditor(html);
 
-  let focusEditor;
-  let showEditorState = true;
-  let editorState = createExtendedThreeRichTextEditor(html);
+function handleChange(event) {
+  editorState = event.detail.editorState;
+}
 
-  function handleChange(event) {
-    editorState = event.detail.editorState;
-  }
+function clearEditor(event) {
+  editorState = clear(editorState);
+  focusEditor();
+}
 
-  function clearEditor(event) {
-    editorState = clear(editorState);
-    focusEditor();
-  }
+function resetEditor(event) {
+  editorState = createExtendedThreeRichTextEditor(html);
+  focusEditor();
+}
 
-  function resetEditor(event) {
-    editorState = createExtendedThreeRichTextEditor(html);
-    focusEditor();
-  }
+function showHtml(event) {
+  alert(toHTML(editorState));
+}
 
-  function showHtml(event) {
-    alert(toHTML(editorState));
-  }
+function showText(event) {
+  alert(toPlainText(editorState));
+}
 
-  function showText(event) {
-    alert(toPlainText(editorState));
-  }
-
-  onMount(() => focusEditor());
+onMount(() => focusEditor());
 
 </script>
 
 <ProsemirrorEditor
-  {editorState}
-  bind:focus={focusEditor}
-  on:change={handleChange}
-  placeholder="Go ahead and edit me!"/>
+{editorState}
+bind:focus={focusEditor}
+on:change={handleChange}
+placeholder="Go ahead and edit me!"/>
 
 <div class="controls">
-  <button on:click={clearEditor}>Clear</button>
-  <button on:click={resetEditor}>Reset</button>
-  <button on:click={showHtml}>Show HTML</button>
-  <button on:click={showText}>Show Text</button>
+<button on:click={clearEditor}>Clear</button>
+<button on:click={resetEditor}>Reset</button>
+<button on:click={showHtml}>Show HTML</button>
+<button on:click={showText}>Show Text</button>
 </div>
 
 <p>
-  <label>Show serialized editor state
-    <input type="checkbox" bind:checked={showEditorState}/>
-  </label>
-</p>
-
-<p>
-  information about html <a href='https://www.w3schools.com/tags/tag_ruby.asp' rel="nofollow external" target="_blank"><code>ruby</code></a> tag.
+<label>Show serialized editor state
+  <input type="checkbox" bind:checked={showEditorState}/>
+</label>
 </p>
 
 {#if showEditorState}
