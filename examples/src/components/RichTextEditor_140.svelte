f17111edaa8c99d67450c98b13c8ea1a3e782393
tidiview <francois.vidit@laposte.net>
2021-06-06 08:52:00 +0200

    fixed &#16; into nbsp;, added <section id=>,

--- examples/src/components/RichTextEditor_140.svelte
+++ examples/src/components/RichTextEditor_140.svelte
@@ -21,11 +21,11 @@
 
 <h3 class="green">明治維新時代の日本カトリック教徒たちには、「大天使ミシェル」でした</h3>
 
-<p>明治維新の日本では、新政府が<a href="https://ja.wikipedia.org/wiki/信仰の自由" title="https://ja.wikipedia.org/wiki/信仰の自由">信仰の自由</a>を認めて、カトリック教会のローマ教皇庁はフランスに本部を置く<a href="https://ja.wikipedia.org/wiki/パリ外国宣教会" title="https://ja.wikipedia.org/wiki/パリ外国宣教会"><ruby>パリ外国宣教会<rp>(</rp><rt>Missions&#160;Étrangères&#160;de&#160;Paris</rt><rp>)</rp></ruby></a>に日本への宣教師派遣を依頼したため、カトリック教徒たちには、フランス語なりの発音である「大天使<ruby>ミシェル<rp>(</rp><rt>Michel</rt><rp>)</rp></ruby>」として知られました。</p>
+<p>明治維新の日本では、新政府が<a href="https://ja.wikipedia.org/wiki/信仰の自由" title="https://ja.wikipedia.org/wiki/信仰の自由">信仰の自由</a>を認めて、カトリック教会のローマ教皇庁はフランスに本部を置く<a href="https://ja.wikipedia.org/wiki/パリ外国宣教会" title="https://ja.wikipedia.org/wiki/パリ外国宣教会"><ruby>パリ外国宣教会<rp>(</rp><rt>Missions&nbsp;Étrangères&nbsp;de&nbsp;Paris</rt><rp>)</rp></ruby></a>に日本への宣教師派遣を依頼したため、カトリック教徒たちには、フランス語なりの発音である「大天使<ruby>ミシェル<rp>(</rp><rt>Michel</rt><rp>)</rp></ruby>」として知られました。</p>
 
 <h3 class="green">新共同訳によって、正式に公に「大天使ミカエル」となりました</h3>
 
-<p>２０世紀後半以降、近代以降の<a href="https://ja.wikipedia.org/wiki/エキュメニズム" title="https://ja.wikipedia.org/wiki/エキュメニズム"><ruby>教会一致運動<rp>(</rp><rt>エキュメニズム</rt><rp>)</rp></ruby></a>の流れに沿って、それまで異なる聖書を用いてきたカトリック教会とプロテスタント諸派世界各国で共同して聖書の翻訳に取り組んだ。</p>
+<p>二十世紀後半以降、近代以降の<a href="https://ja.wikipedia.org/wiki/エキュメニズム" title="https://ja.wikipedia.org/wiki/エキュメニズム"><ruby>教会一致運動<rp>(</rp><rt>エキュメニズム</rt><rp>)</rp></ruby></a>の流れに沿って、それまで異なる聖書を用いてきたカトリック教会とプロテスタント諸派世界各国で共同して聖書の翻訳に取り組んだ。</p>
 <p>日本でもこの流れを受けて超教派のメンバーからなる翻訳委員会が結成され、まず1978年に共同訳聖書（新約のみ）を世に問うた。</p>
 <p>しかし、共同訳聖書は「イエスス」「パウロス」などの固有名詞の原音表記主義や、翻訳が意訳すぎて従来親しまれた言葉が消えてしまったなどのために評判がよくなく、あまり受け入れられなかったため、新たな共同訳聖書の翻訳が行われた。</p>
 <p>これが新共同訳聖書である。</p>
@@ -33,16 +33,18 @@
 
 <h3 class="green">ヘブライ語での大天使ミカエルの発音とその意味とは</h3>
 
-<h4 id="sens-en-hébreu" class="green">ヘブライ語での意味</h4>
+<section id="sens-en-hebreu">
+<h4 class="green">ヘブライ語での意味</h4>
 
 <p>時代によって、呼び名が異なってきましたが、指されているものは今も昔も一緒です。</p>
-<p>元々は聖書の原文であるヘブライ語での大天使の名は「 <ruby><rb><span style="background:#f1f9f1;color:#3d8b3d;" lang="he">מיכאל</span></rb><rt><span style="background:#f1f9f1;color:#3d8b3d;" lang="ja">ミッハエル</span></rt></ruby> 」となっています。</p>
+<p>元々は聖書の原文であるヘブライ語での大天使の名は「&nbsp;<ruby><rb><span style="background:#f1f9f1;color:#3d8b3d;" lang="he">מיכאל</span></rb><rt><span style="background:#f1f9f1;color:#3d8b3d;" lang="ja">ミッハエル</span></rt></ruby>&nbsp;」となっています。</p>
+</section>
 
 <h4 class="green">ヘブライ語での発音</h4>
 
 <p>ここでは、カタカナでの音の書き直しは、<a href="https://ja.wikipedia.org/wiki/国際音声記号" title="https://ja.wikipedia.org/wiki/国際音声記号">国際音声記号</a>で「Mîḵā’ēl」との発音に値することは、なかなか適するのはないため、次のヘブライ語での発音の録音を用意しました。</p>
 
-<p class="no-puce"><audio controls="1" title="ヘブライ語での מיכאל の発音" alt="ヘブライ語での מיכאל の発音"><source src="https://francois-vidit.com/user/sites/docs/pages/01.home/05.mont-saint-michel/01.arch-michel/01.nom/prononciation_mihael.mp3" type="audio/mpeg">あなたのブラウザーは <code>audio</code> 要素に対応していません。</audio></p>
+<p class="no-puce"><audio controls title="ヘブライ語での מיכאל の発音"><source src="https://francois-vidit.com/user/sites/docs/pages/01.home/05.mont-saint-michel/01.arch-michel/01.nom/prononciation_mihael.mp3" type="audio/mpeg">あなたのブラウザーは <code>audio</code> 要素に対応していません。</audio></p>
 
 <h2 class="green">「神に似たるものは誰か」との意味の名</h2>
 
@@ -51,20 +53,20 @@
 <p>「<ruby><rb><span style="background:#f1f9f1;color:#3d8b3d;">מיכאל</span></rb><rt><span style="background:#f1f9f1;color:#3d8b3d;">ミッハエル</span></rt></ruby>」とのヘブライ語を分析しますと：</p>
 <p>
   <span style="background:#f1f9f1;color:#3d8b3d;min-width:50px;float:left;text-align:center;">mî</span>
-  <span style="background:#fff;min-width:10px;float:left;">&nbsp</span>
+  <span style="background:#fff;min-width:10px;float:left;">&nbsp;</span>
   <span style="background:#f1f9f1;color:#3d8b3d;min-width:60px;float:left;text-align:center;">ḵā’</span>
-  <span style="background:#fff;min-width:60px;float:left;">&nbsp</span>
+  <span style="background:#fff;min-width:60px;float:left;">&nbsp;</span>
   <span style="background:#f1f9f1;color:#3d8b3d;min-width:50px;float:left;text-align:center;">ēl</span>
 </p>
 <p>
   <span style="background:#f1f9f1;color:#3d8b3d;min-width:50px;float:left;text-align:center;">誰か</span>
-  <span style="background:#fff;min-width:10px;float:left;">&nbsp</span>
+  <span style="background:#fff;min-width:10px;float:left;">&nbsp;</span>
   <span style="background:#f1f9f1;color:#3d8b3d;min-width:60px;float:left;text-align:center;">似たる</span>
-  <span style="background:#fff;min-width:10px;float:left;">&nbsp</span>
+  <span style="background:#fff;min-width:10px;float:left;">&nbsp;</span>
   <span style="background:#fff;min-width:40px;float:left;">もの</span>
-  <span style="background:#fff;min-width:10px;float:left;">&nbsp</span>
+  <span style="background:#fff;min-width:10px;float:left;">&nbsp;</span>
   <span style="background:#f1f9f1;color:#3d8b3d;min-width:50px;float:left;text-align:center;">神</span>
-  <span style="background:#fff;min-width:10px;float:left;">&nbsp</span>
+  <span style="background:#fff;min-width:10px;float:left;">&nbsp;</span>
   <span style="background:#fff;min-width:40px;float:left;">に</span>
 </p>
 <p>逆順に直して、「神に似たるものは誰か」との意味になります！</p>
@@ -77,7 +79,7 @@
 <p>「<ruby>モン<rp>(</rp><rt>Mont</rt><rp>)</rp>・<rp>(</rp><rt></rt><rp>)</rp>サン<rp>(</rp><rt>Saint</rt><rp>)</rp>＝<rp>(</rp><rt>-</rt><rp>)</rp>ミシェル<rp>(</rp><rt>Michel</rt><rp>)</rp></ruby>」を分析しますと：</p>
 <p>
   <span style="background:#f1f9f1;color:#3d8b3d;min-width:50px;float:left;text-align:center;">Mont</span>
-  <span style="background:#fff;min-width:20px;float:left;">&nbsp</span>
+  <span style="background:#fff;min-width:20px;float:left;">&nbsp;</span>
   <span style="background:#f1f9f1;color:#3d8b3d;min-width:50px;float:left;text-align:center;">Saint</span>
   <span style="background:#fff;min-width:20px;float:left;text-align:center;">-</span>
   <span style="background:#f1f9f1;color:#3d8b3d;min-width:70px;float:left;text-align:center;">Michel</span>
